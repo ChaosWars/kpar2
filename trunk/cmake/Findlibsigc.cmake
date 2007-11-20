b16c3a9ccb68532900ccf3097a86581674e28cfa
valheru.ashen.shugar <valheru.ashen.shugar@9d17a2d6-373f-0410-89e7-d30cb3578592>
2007-11-20 07:04:05 +0000

    Changed install path forkpar2.desktop

---
+++ trunk/cmake/Findlibsigc.cmake
@@ -0,0 +1,19 @@
+FIND_PATH( LIBSIGC_INCLUDE_DIR sigc++/sigc++.h /usr/include/sigc++-2.0/ /usr/local/include/sigc++-2.0 )
+FIND_PATH( LIBSIGC_LIB_INCLUDE_DIR sigc++config.h /usr/lib/sigc++-2.0/include /usr/local/lib/sigc++-2.0/include )
+FIND_LIBRARY( LIBSIGC_LIBRARY NAMES sigc-2.0 PATHS /usr/lib /usr/local/lib )
+SET( LIBSIGC_INCLUDE_DIRS ${LIBSIGC_INCLUDE_DIR} ${LIBSIGC_LIB_INCLUDE_DIR} )
+
+IF( LIBSIGC_INCLUDE_DIRS AND LIBSIGC_LIBRARY )
+   SET( LIBSIGC_FOUND TRUE )
+ENDIF( LIBSIGC_INCLUDE_DIRS AND LIBSIGC_LIBRARY )
+
+
+IF( LIBSIGC_FOUND )
+   IF( NOT libsigc_FIND_QUIETLY )
+      MESSAGE( STATUS "Found libsigc: ${LIBSIGC_LIBRARY}" )
+   ENDIF( NOT libsigc_FIND_QUIETLY )
+ELSE( LIBSIGC_FOUND )
+   IF( libsigc_FIND_REQUIRED )
+      MESSAGE( FATAL_ERROR "Could not find libsigc" )
+   ENDIF( libsigc_FIND_REQUIRED )
+ENDIF( LIBSIGC_FOUND )
